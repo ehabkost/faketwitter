9b6a89221a39ac50df1431f42c301c2b22d08ae2
Eduardo Habkost <ehabkost@raisama.net>
2010-02-12 02:39:41 -0200

    simple fake timing code
    Signed-off-by: Eduardo Habkost <ehabkost@raisama.net>

--- faketwitter/server.py
+++ faketwitter/server.py
@@ -5,16 +5,30 @@ from faketwitter import objects
 class TwitterServer:
     def __init__(self):
         self._all_statuses = []
+        self._cur_time = 0
 
-    def _new_tatus(self, st):
+    def cur_time(self):
+        return self._cur_time
+
+    def set_time(self, t):
+        self._cur_time = t
+
+    def advance_time(self, delta):
+        self._cur_time += delta
+
+    def _new_status(self, st):
         self._all_statuses.append(st)
-        
+
     def new_status(self, user, text):
         s = objects.Status()
+
+        #FIXME: use formatted time
+        s.add('created_at', str(self.cur_time()))
         s.add('text', text)
         u = s.add('user')
         u.add('screen_name', user)
-        self._new_tatus(s)
+
+        self._new_status(s)
 
     def public_timeline(self, count):
         return self._all_statuses[-count:]
